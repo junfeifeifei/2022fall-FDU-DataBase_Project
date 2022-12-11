3ec0b73746daabe96c2984524353b0425f0e3d2b
Mai Wuyuao <20300290032@fudan.edu.cn>
2022-12-11 15:11:18 +0800

    修改了部分页面导航的逻辑

--- index.php
+++ index.php
@@ -1,38 +1,38 @@
 <?php
 error_reporting(0);
+session_start();
 $url = $_SERVER['REQUEST_URI'];
 $urlAfter=parse_url($url);
 $path=$urlAfter['path'];
-$type = "";
-
-if(!isset($GLOBALS["initialized"])){
-    $type = $_GET["type"];
-    require 'Controllers/initialMysql.php';
+if(!isset($_SESSION['initialized'])){
+    require './Controllers/initialMysql.php';
     initial_mysql();
+    $_SESSION['initialized']=1;
+    echo "<script>window.location.href='/';</script>";
 }
 else{
-    $GLOBALS["initialized"]=1;
-    if(isset($_GET["type"])){
+    if(!isset($_GET["type"])){//这里面代表的是页面显示部分
+        if($path == '/'){
+            require 'Views/HTML/Login.html';
+        }
+        if($path == '/student'){
+            require 'Views/HTML/Student/StudentMain.html';
+        }
+        if($path == '/student/leaveapply'){
+            require 'Views/HTML/Student/StudentLeaveApply.html';
+        }
+        if($path == '/student/enterapply'){
+            require 'Views/HTML/Student/StudentEnterApply.html';
+        }
+    }
+    else{//这里开始是功能的判断
         $type = $_GET["type"];
-    }
-//对于跳转类型的判断
-    if($type == "login"){
-        require 'Controllers/LoginController.php';
-    }
-    if($type == "returnToStudent"){
-        echo "<script>window.location.href='/student';</script>";
-    }
-    if($path == '/'){
-        require 'Views/HTML/Login.html';
-    }
-//学生的界面跳转
-    if($path == '/student'){
-        require 'Views/HTML/Student/StudentMain.html';
-    }
-    if($path == '/student/leaveapply'){
-        require 'Views/HTML/Student/StudentLeaveApply.html';
-    }
-    if($path == '/student/enterapply'){
-        require 'Views/HTML/Student/StudentEnterApply.html';
+        //登录功能
+        if($type == "login"){
+            require 'Controllers/LoginController.php';
+        }
+        if($type == "returnToStudent"){
+            echo "<script>window.location.href='/student';</script>";
+        }
     }
 }
